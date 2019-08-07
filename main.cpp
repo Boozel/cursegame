6678359829684086f8358182cf715934f895fc68
Gerritt <Gerrittdorland@gmail.com>
2019-08-07 14:43:52 -0700

    debug message working. can not detect max x/y

--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
 #include <ncurses.h>
 #include "player_char.h"
-
+#include <string.h>
 #define KEY_ESC 27
 
+void printmesg(char* text, int maxy, int maxx);
+void itos(int input, char* out);
 int main(void)
 {
    Player player;
    int ch = 0;
+	int y, x;
+	char debug[25]={'t','e','s','t','\0'};
 
    initscr();
    raw();
    noecho();
    cbreak();
-   keypad(stdscr, TRUE);
-   
+   keypad(stdscr, TRUE); 
+	getmaxyx(stdscr, y, x);
 
    while(ch != KEY_ESC)
    {
@@ -25,8 +29,19 @@ int main(void)
       }
 
       player.UpdatePlayer();
-      refresh();
+		printmesg(debug, y, x);
+		refresh();
    }
    endwin();
    return 0;
+}
+
+void printmesg(char* text, int maxy, int maxx)
+{
+	mvprintw(maxy-1, 0, text);
+}
+
+void itos(int in, char *out)
+{
+	sprintf(out, "%d", in);
 }
